c6116593aceadf1d68ddf0ce5ccfafffa1ae1401
Heather Nuffer <zzzuzz@gmail.com>
2020-01-11 21:28:02 -0700

    Started motd route

--- api/server.js
+++ api/server.js
@@ -19,6 +19,19 @@ server.get('/', (req, res) => {
   });
 });
 
+server.get('/motd', (req, res) => {
+  Shoutouts.find()
+  .then(shoutouts => {
+    res.status(200).json(shoutouts);
+  })
+  .catch (error => {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  });
+});
+
+
+
 server.post('/', (req, res) => {
   Shoutouts.add(req.body)
   .then(shoutout => {
